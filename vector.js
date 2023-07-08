3b7aed9f3a51f648496fd15c800c7a9e0e536c7f
Noreh AD <akinoreh@gmail.com>
2023-07-08 23:42:30 +0300

    vector: new methods

--- vector.js
+++ vector.js
@@ -1,7 +1,7 @@
 // https://en.wikipedia.org/wiki/Vector_(mathematics_and_physics)
 class Vector {
     
-    constructor(x, y) {
+    constructor(x = 0, y = 0) {
         this.x = x;
         this.y = y;
     }
@@ -18,20 +18,50 @@ class Vector {
         return this;
     }
     
-    scale(scalar) {
+    multiply(scalar) {
         this.x *= scalar;
         this.y *= scalar;
         return this;
     }
     
-    magnitude() {
-        return Math.sqrt(this.x * this.x + this.y * this.y);
+    divide(scalar) {
+        this.x /= scalar;
+        this.y /= scalar;
+        return this;
     }
     
     normalize() {
         const mag = this.magnitude();
-        this.x /= mag;
-        this.y /= mag;
+        if (mag != 0) this.divide(mag);
+        return this;
+    }
+    
+    getMagnitude(value) {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+    
+    setMagnitude(value) {
+        this.normalize().multiply(value);
+    }
+    
+    getAngle(reference = new Vector(1, 0)) {
+        const dotProduct = Vector.dot(
+            Vector.createCopy(this).normalize(),
+            Vector.createCopy(reference).normalize(),
+        );
+        return rad2deg( Math.acos(dotProduct) );
+    }
+    
+    setAngle(value) {
+        let angle = deg2rad(value);
+        let mag = this.getMagnitude();
+        this.x = Math.cos(angle) * mag;
+        this.y = Math.sin(angle) * mag;
+        return this;
+    }
+    
+    rotate(value) {
+        this.setAngle(this.getAngle() + value);
         return this;
     }
     
@@ -39,16 +69,23 @@ class Vector {
         return vector1.x * vector2.x + vector1.y * vector2.y;
     }
     
-    static copy(vector) {
+    static createCopy(vector) {
         return new Vector(vector.x, vector.y);
     }
     
-    angle(reference = new Vector(1, 0)) {
-        const dotProduct = Vector.dot(
-            Vector.copy(this).normalize(),
-            Vector.copy(reference).normalize(),
+    static createFromAngle(angle, magnitude) {
+        let x = Math.cos( deg2rad(angle) );
+        let y = Math.sin( deg2rad(angle) );
+        let v = new Vector(x, y);
+        v.magnitude(magnitude);
+        return v;
+    }
+    
+    static createFromPoints(point1, point2) {
+        return Vector.createFromAngle(
+            point2.angle(point1),
+            point2.distance(point1),
         );
-        return Math.acos(dotProduct) * (180 / Math.PI);
     }
     
 }
